3c7d9b4444f07c0cdbc4f4eaca2d7a42445a55a6
ChrisG3D <christian.e.guevara@gmail.com>
2021-04-06 16:36:09 +0200

    Add functions to add, delete pokemon from own team, retrieve user's team_id and the existing teams

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Model\UserManager;
 use App\Model\TeamManager;
 use App\Model\PokemonManager;
-use Exception;
 
 class UserController extends AbstractController
 {
@@ -29,5 +28,65 @@ class UserController extends AbstractController
         $trainers = $this->userManager->selectByRole(2);
 
         return $this->twig->render('User/list.html.twig', ['trainers' => $trainers]);
+    }
+
+    /**
+     * Delete a trainer
+     */
+    public function delete(int $id)
+    {
+        // Getting the rowCount value which is returned at the end of deleteTrainerById function
+        $rowCount = $this->userManager->deleteTrainerById($id);
+        $validationMessage = $rowCount == 1 ? 'Le trainer a bien été supprimé!' : 'erreur!';
+        // créer une variable de session $_SESSION['dlt_trainer_msg']
+        $_SESSION['dlt_trainer_msg'] = $validationMessage;
+        header('Location: /User/list');
+        //
+    }
+
+    /**
+     * Add a pokemon to a team
+     */
+
+    public function addPokemon()
+    {
+        $teamId = $this->userManager->selectTeamIdByUserId($_SESSION['id']);
+        $pokemons = $this->pokemonManager->selectAllWithAttackTypes();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $errors = [];
+            $pokemonId = $_POST['pokemon_id'];
+
+            //Checking if pokemon exist in database if not null
+            $pokemon_exist = false;
+            foreach ($pokemons as $pokemon) {
+                if ($pokemon['id'] === $pokemonId) {
+                    $pokemon_exist = true;
+                }
+            }
+
+            if (!$pokemon_exist && trim($pokemonId) !== "") {
+                $errors[] = "The selected pokemon does not exist. Please select a correct value.";
+            } else {
+                $this->teamManager->addPokemonToTeam($pokemonId, $teamId[0]);
+            }
+        }
+    }
+
+    /**
+     * Delete pokemon from a team
+     */
+
+    public function deletePokemon($pokemonId, $teamId)
+    {
+        $teamId = $_SESSION['id'];
+        $pokemonId = $_POST['pokemon_id'];
+        // Getting the rowCount value which is returned at the end of deletePokemonFromTeam function
+        $rowCount = $this->teamManager->deletePokemonFromTeam($pokemonId, $teamId);
+        $validationMessage = $rowCount == 1 ? 'Le pokémon a bien été retiré de l\'équipe!' : 'erreur!';
+        // créer une variable de session $_SESSION['dlt_pokTeam_msg']
+        $_SESSION['dlt_pokTeam_msg'] = $validationMessage;
+        header('Location: /Pokemon/list');
+        //
     }
 }
--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -9,16 +9,13 @@ class UserManager extends AbstractManager
     public function selectByUsernamePassword(string $name, string $password)
     {
         // prepared request
-        $statement = $this->pdo->prepare(
-            "SELECT * FROM " . static::TABLE . " WHERE name = :name AND password = :password"
-        );
+        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE name = :name AND password = :password");
         $statement->bindValue('name', $name, \PDO::PARAM_STR);
         $statement->bindValue('password', $password, \PDO::PARAM_STR);
         $statement->execute();
 
         return $statement->fetch();
     }
-
 
     /**
      * Select all users with trainer role
@@ -31,6 +28,32 @@ class UserManager extends AbstractManager
         $statement->bindValue('roleId', $roleId, \PDO::PARAM_INT);
         $statement->execute();
 
+        return $statement->fetchAll();
+    }
+
+    /*     Delete trainer */
+    public function deleteTrainerById(int $id)
+    {
+        $query = 'DELETE FROM User WHERE id = :id';
+        // :id to bind with bind value
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        // returns lines which have been affected by an INSERT, UPDATE or DELETE
+        return $statement->rowCount();
+    }
+
+    /**
+     * Get user's team_id by its own id
+     * @param int $userId
+     */
+    public function selectTeamIdByUserId(int $userId)
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("SELECT team_id FROM " . static::TABLE . " WHERE id =:userId");
+        $statement->bindValue('userId', $userId, \PDO::PARAM_INT);
+        $statement->execute();
+        
         return $statement->fetchAll();
     }
 }
