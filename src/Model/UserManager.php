9410c1f2fee0ed716be39f6a4d705ac2d8bb681b
ChrisG3D <christian.e.guevara@gmail.com>
2021-04-06 13:41:51 +0200

    Add add and delete pokemon to team on UserManager

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -16,4 +16,35 @@ class UserManager extends AbstractManager
 
         return $statement->fetch();
     }
+
+
+    /**
+     * Add pokemon to team with IDs
+     * @param $pokemonId
+     * @param $teamId
+     */
+    public function addPokemonToTeam(int $pokemonId, int $teamId)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO Pokemon_Team (`pokemon_id`, `team_id`) VALUES (:pokemon_id, :team_id)");
+        $statement->bindValue('pokemon_id', $pokemonId, \PDO::PARAM_INT);
+        $statement->bindValue('team_id', $teamId, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+
+    /**
+     * Delete Pokemon from team with IDs
+     * @param $pokemonId
+     * @param $teamId
+     */
+    public function deletePokemonFromTeam(int $pokemonId, int $teamId)
+    {
+        $query = 'DELETE FROM Pokemon_Team WHERE pokemon_id = :pokemon_id AND team_id = :team_id';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':pokemon_id', $pokemonId, \PDO::PARAM_INT);
+        $statement->bindValue(':team_id', $teamId, \PDO::PARAM_INT);
+        $statement->execute();
+        // returns lines which have been affected by an INSERT, UPDATE or DELETE
+        return $statement->rowCount();
+    }
 }
